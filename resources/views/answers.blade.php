930faf138a15053b7d17a5c3f4128ff2583edd52
adanlaldy1 <adanlaldy@gmail.com>
2025-01-17 17:58:21 +0100

    fix: header

--- resources/views/answers.blade.php
+++ resources/views/answers.blade.php
@@ -6,17 +6,19 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-200">
-<header class="flex justify-between items-center text-4xl font-semibold py-8 px-14 bg-gray-700 text-white">
-    <div>
-        <a href="{{ route('home') }}">Survey Project</a>
-    </div>
-    <div class="space-x-16 flex">
-        <a href="{{ route('get.login') }}" class="flex size-14">
-            <img src="/images/add.png" alt="new survey button">
-        </a>
-        <a href="{{ route('logout') }}" class="flex size-16">
-            <img src="/images/logout.png" alt="logout button">
-        </a>
+<header class="text-4xl font-semibold py-8 px-14 bg-gray-700 text-white">
+    <div class="flex items-center justify-between w-full">
+        <div class="flex justify-center w-full pl-14">
+            <a href="{{ route('home') }}">Survey Project</a>
+        </div>
+        <div class="flex justify-end space-x-8 items-center">
+            <a href="{{ route('get.login') }}" class="flex size-14">
+                <img src="/images/add.png" alt="new survey button">
+            </a>
+            <a href="{{ route('logout') }}" class="flex size-16">
+                <img src="/images/logout.png" alt="logout button">
+            </a>
+        </div>
     </div>
 </header>
 <main class="pt-6">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,17 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-200">
-<header class="flex justify-between text-4xl font-semibold py-11 px-14 bg-gray-700 text-white">
-    <div>
-        <a href="{{ route('home') }}">Survey Project</a>
-    </div>
-    <div class="space-x-16">
-        <a href="{{ route('get.register') }}" class="text-center rounded-lg text-lg py-2 px-2 bg-blue-700 text-white hover:bg-blue-600 shadow-lg">Register</a>
-        <a href="{{ route('get.login') }}" class="text-center rounded-lg text-lg py-2 px-2 bg-blue-700 text-white hover:bg-blue-600 shadow-lg">Log in</a>
+<header class="text-4xl font-semibold py-10 px-14 bg-gray-700 text-white">
+    <div class="flex items-center justify-between w-full">
+        <div class="flex justify-center w-full pl-20">
+            <a href="{{ route('home') }}">Survey Project</a>
+        </div>
+        <div class="flex justify-end space-x-8 items-center">
+            <a href="{{ route('get.register') }}" class="text-center rounded-lg text-lg py-2 px-2 bg-blue-700 text-white
+            hover:bg-blue-600 shadow-lg">Register</a>
+            <a href="{{ route('get.login') }}"
+               class="text-center rounded-lg text-lg py-2 px-2 bg-blue-700 text-white hover:bg-blue-600 shadow-lg">Login</a>
+        </div>
     </div>
 </header>
 <main class="pt-16">
