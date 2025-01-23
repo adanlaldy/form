9bdac0001651673cbca49dd70d8975e2d7660b22
adanlaldy1 <adanlaldy@gmail.com>
2025-01-23 13:06:14 +0100

    feat: label in the header

--- resources/views/components/connected_header.blade.php
+++ resources/views/components/connected_header.blade.php
@@ -1,13 +1,14 @@
-<header class="text-4xl font-semibold py-8 px-14 bg-gray-700 text-white">
+<header class="text-4xl font-semibold py-6 px-14 bg-gray-700 text-white">
     <div class="flex items-center justify-between w-full">
-        <div class="flex justify-center w-full pl-32">
+        <div class="flex justify-center w-full pl-44 hover:underline">
             <a href="{{ route('all.surveys') }}">Survey Project</a>
         </div>
-        <div class="flex justify-end space-x-8 items-center">
-            <a id="new_survey" href="{{ route('get.survey') }}" class="flex size-14">
+        <div class="flex justify-end space-x-10 items-center">
+            <a id="new_survey" href="{{ route('get.survey') }}" class="flex flex-col size-20">
                 <img src="/images/contact-form.png" alt="new survey button">
+                <label class="text-xs">New survey</label>
             </a>
-            <a id="logout" href="{{ route('logout') }}" class="flex size-16">
+            <a id="logout" href="{{ route('logout') }}" class="flex size-20">
                 <img src="/images/logout.png" alt="logout button">
             </a>
         </div>
